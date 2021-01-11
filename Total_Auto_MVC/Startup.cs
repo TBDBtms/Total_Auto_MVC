2e54bbe5859aa193a6f1b32cc99fd43fa71bae54
hzx <hzx@DESKTOP-VC91AIV>
2021-01-11 15:28:21 +0800

    984984954952

--- Total_Auto_MVC/Startup.cs
+++ Total_Auto_MVC/Startup.cs
@@ -46,7 +46,7 @@ namespace Total_Auto_MVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Hzx}/{action=Index}/{id?}");
             });
         }
     }
