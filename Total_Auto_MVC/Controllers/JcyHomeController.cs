0224823b1baf60a3a3edf4e86f068c2e18d8cec8
超宇 晋 <1042463806@qq.com>
2021-01-14 15:06:01 +0800

    1111111

--- Total_Auto_MVC/Controllers/JcyHomeController.cs
+++ Total_Auto_MVC/Controllers/JcyHomeController.cs
@@ -12,8 +12,9 @@ namespace Total_Auto_MVC.Controllers
         {
             return View();
         }
-        public IActionResult ShowCard()
+        public IActionResult ShowCard(int id=0)
         {
+            ViewBag.id = id;
             return View();
         }
     }
