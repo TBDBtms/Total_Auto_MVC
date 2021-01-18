0c94cf589d3829d8a79a3e48f03b3cc750d9960b
zhq1211 <2982957828@qq.com>
2021-01-18 11:44:13 +0800

    123

---
+++ Total_Auto_MVC/Controllers/ZHQ_GJController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Total_Auto_MVC.Controllers
+{
+    public class ZHQ_GJController : Controller
+    {
+        public IActionResult GJ()
+        {
+            return View();
+        }
+        public IActionResult PP()
+        {
+            return View();
+        }
+        public IActionResult CX()
+        {
+            return View();
+        }
+        public IActionResult KS()
+        {
+            return View();
+        }
+        public IActionResult GJXX()
+        {
+            return View();
+        }
+    }
+}
