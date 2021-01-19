b9a431de9ed5d78da2514eb89a3fc90a58534bd9
林虎 杜 <1661392274@qq.com>
2021-01-19 09:27:39 +0800

    ddd

---
+++ Total_Auto_MVC/Controllers/DLH_TXController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Total_Auto_MVC.Models;
+
+namespace Total_Auto_MVC.Controllers
+{
+    public class DLH_TXController : Controller
+    {
+        UserLogin users = new UserLogin();
+        public IActionResult Login()
+        { 
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    
+    }
+}
